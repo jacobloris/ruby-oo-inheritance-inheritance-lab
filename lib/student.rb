4ff644015e461d1279a921fb819752c4c00f6b44
jacobloris <jacobloris@outlook.com>
2022-01-19 13:00:08 -0700

    commit tree 969f842f6b97dcecce4ca0bed9967bdf8266723b

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+
+    attr_reader :knowledge
+    attr_accessor :first_name, :last_name
+
+    def initialize 
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
 
 end
